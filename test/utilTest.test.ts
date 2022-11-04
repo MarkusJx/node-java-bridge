e14380e1edf5e7ac94e8e5bce42b46393fde2e22
Markus <28785953+MarkusJx@users.noreply.github.com>
2022-11-04 13:22:25 +0100

    chore: release 2.1.5 (#28)

--- test/utilTest.test.ts
+++ test/utilTest.test.ts
@@ -1,15 +1,21 @@
 import { getJavaVersion, getJavaVersionSync } from '../.';
 import { it } from 'mocha';
 import { expect } from 'chai';
+import isCI from 'is-ci';
+
+let timeout = 2e3;
+if (isCI && (process.arch === 'arm' || process.arch === 'arm64')) {
+    timeout = 20e3;
+}
 
 describe('util test', () => {
     it('Get java version', async () => {
         const version = await getJavaVersion();
         expect(version).to.be.a('string').to.have.length.greaterThanOrEqual(1);
-    });
+    }).timeout(timeout);
 
     it('Get java version (sync)', () => {
         const version = getJavaVersionSync();
         expect(version).to.be.a('string').to.have.length.greaterThanOrEqual(1);
-    });
+    }).timeout(timeout);
 });
