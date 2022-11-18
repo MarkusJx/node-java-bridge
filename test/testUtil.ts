e61dcddb9da9111af4da0891b229e2f4f7a2a472
MarkusJx <28785953+markusjx@users.noreply.github.com>
2022-11-18 15:12:18 +0100

    ci: update tests
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- test/testUtil.ts
+++ test/testUtil.ts
@@ -2,3 +2,6 @@ import isCi from 'is-ci';
 
 export const shouldIncreaseTimeout =
     isCi && (process.arch === 'arm64' || process.arch === 'arm');
+
+console.log('Process arch:', process.arch);
+console.log('Process platform:', process.platform);
